c070c0a0caa987efbcc132c6d999b7ffeebb08e0
Chen Kang Chong <s10189088@connect.np.edu.sg>
2020-01-16 14:05:10 +0800

    Nav bar, MosqXS's mainpage

--- MosqXS/Startup.cs
+++ MosqXS/Startup.cs
@@ -15,7 +15,7 @@ namespace MosqXS
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(); // service that reusable in website
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,7 +37,7 @@ namespace MosqXS
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=MosqXSMain}/{id?}");
             });
         }
     }
