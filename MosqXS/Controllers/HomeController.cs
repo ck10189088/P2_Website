1712b3cb7d3c52b613f0b41cedeb52a1d063a1f3
Ainsley Malcolm Pereira <s10186606@connect.np.edu.sg>
2020-01-30 09:57:54 +0800

    Info Page

--- MosqXS/Controllers/HomeController.cs
+++ MosqXS/Controllers/HomeController.cs
@@ -22,5 +22,10 @@ namespace MosqXS.Controllers
         {
             return RedirectToAction("MosqXSHotSpot");
         }
+
+        public IActionResult MosqXSInfo()
+        {
+            return View();
+        }
     }
 }
